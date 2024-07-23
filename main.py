5d163f74ad753688553786f9e14670fb65ed9997
Dev Joshi <GalaxiDev0@gmail.com>
2024-07-23 18:44:58 -0400

    reduced training time and organized code

--- main.py
+++ main.py
@@ -4,44 +4,41 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.classify import NaiveBayesClassifier
 from nltk.classify.util import accuracy
-import string
 
-# Download necessary NLTK data
 nltk.download('punkt')
 nltk.download('stopwords')
 
-# Load CSV file
-df = pd.read_csv('IMDB Dataset.csv')
+data_frame = pd.read_csv('IMDB Dataset.csv')
 
-# Preprocess the data
+
 stop_words = set(stopwords.words('english'))
 
+
 def preprocess_text(text):
-    # Tokenize the text
     words = word_tokenize(text)
-    # Remove punctuation and stop words, and convert to lowercase
-    words = [word.lower() for word in words if word.isalpha() and word.lower() not in stop_words]
-    return words
+    words = {word.lower() for word in words if word.isalpha() and word.lower() not in stop_words}
+    return list(words)
 
-df['processed_text'] = df['text'].apply(preprocess_text)
+data_frame['processed_text'] = data_frame['text'].apply(preprocess_text)
 
-# Map 'positive' to 1 and 'negative' to 0
+
 label_map = {'positive': 1, 'negative': 0}
-df['label'] = df['label'].map(label_map)
+data_frame['label'] = data_frame['label'].map(label_map)
+
 
 def get_features(words):
     return {word: True for word in words}
 
-# Create a list of tuples for training
-train_data = [(get_features(words), label) for words, label in zip(df['processed_text'], df['label'])]
+train_data = [(get_features(words), label) for words, label in zip(data_frame['processed_text'], data_frame['label'])]
 
-# Train a Naive Bayes classifier
+
 classifier = NaiveBayesClassifier.train(train_data)
 
-# Calculate and print the accuracy of the classifier on the training data
+# Calculate accuracy
 train_accuracy = accuracy(classifier, train_data)
-print(f'Accuracy: {train_accuracy * 100:.2f}%')
+print(f'Training Accuracy: {train_accuracy * 100:.2f}%')
 
+# Analyze sentiment
 def analyze_sentiment(text):
     processed_text = preprocess_text(text)
     features = get_features(processed_text)
@@ -49,7 +46,6 @@ def analyze_sentiment(text):
     sentiment_score = sentiment_prob_dist.prob(1)  # Probability of positive sentiment
     return sentiment_score
 
-# Example usage
-text = "bad movie"
+text = "movie"
 sentiment_score = analyze_sentiment(text)
-print(f'Sentiment score: {sentiment_score * 100:.2f}%')
+print(f'Sentiment Score: {sentiment_score * 100:.2f}%')

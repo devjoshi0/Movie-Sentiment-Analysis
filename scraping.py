6e985aeed944fc052a75c7a5e15832e05bc02360
Dev Joshi <GalaxiDev0@gmail.com>
2024-07-24 14:54:18 -0400

    scraping imdb reviews

--- scraping.py
+++ scraping.py
@@ -8,9 +8,28 @@ import string
 import requests
 from bs4 import BeautifulSoup
 import pandas
+import json
 
 
-user_input = input("Enter the movie name: ")
+user_input = "avatar"
 
-imdb_user_reviews_url = "https://www.imdb.com/title/tt4154796/"
-imdb_critic_reviews_url = ""
+
+
+
+# Scrape reviews from IMDB
+reviews_url = f"https://www.imdb.com/title/tt0499549/reviews/?ref_=tt_ov_rt"
+reviews_response = requests.get(reviews_url)
+reviews_soup = BeautifulSoup(reviews_response.text, 'html.parser')
+review_containers = reviews_soup.find_all('div', class_='review-container')
+
+# Store reviews in a list
+reviews = []
+for container in review_containers:
+    review = container.find('div', class_='text show-more__control').text.strip()
+    reviews.append(review)
+
+# Save reviews to a JSON file
+json_data = json.dumps(reviews[:100])
+with open('reviews.json', 'w') as file:
+    file.write(json_data)
+

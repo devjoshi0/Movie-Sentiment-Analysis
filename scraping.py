7c06399b6a3d2689bb72ba80ef92b52b561c49fd
Dev Joshi <GalaxiDev0@gmail.com>
2024-07-24 23:51:28 -0400

    attempt on scraping given user input

--- scraping.py
+++ scraping.py
@@ -15,21 +15,33 @@ user_input = "avatar"
 
 
 
+# Function to scrape IMDB reviews
+def scrape_imdb_reviews(movie_name):
+    
+    search_url = f"https://www.imdb.com/find?q={movie_name}&s=tt&ttype=ft&ref_=fn_ft"
+    search_response = requests.get(search_url)
+    search_soup = BeautifulSoup(search_response.text, 'html.parser')
+    result_text = search_soup.find('td', class_='result_text')
+    if result_text is not None:
+        movie_id = result_text.find('a')['href'].split('/')[2]
+        print(movie_id)
+    else:
+        movie_id = None  # or handle the case when movie_id is not found
 
-# Scrape reviews from IMDB
-reviews_url = f"https://www.imdb.com/title/tt0499549/reviews/?ref_=tt_ov_rt"
-reviews_response = requests.get(reviews_url)
-reviews_soup = BeautifulSoup(reviews_response.text, 'html.parser')
-review_containers = reviews_soup.find_all('div', class_='review-container')
 
-# Store reviews in a list
-reviews = []
-for container in review_containers:
-    review = container.find('div', class_='text show-more__control').text.strip()
-    reviews.append(review)
+    reviews_url = f"https://www.imdb.com/title/tt0499549/reviews/?ref_=tt_ov_rt"
+    reviews_response = requests.get(reviews_url)
+    reviews_soup = BeautifulSoup(reviews_response.text, 'html.parser')
+    review_containers = reviews_soup.find_all('div', class_='review-container')
 
-# Save reviews to a JSON file
-json_data = json.dumps(reviews[:100])
-with open('reviews.json', 'w') as file:
-    file.write(json_data)
+    reviews = []
+    for container in review_containers:
+        review = container.find('div', class_='text show-more__control').text.strip()
+        reviews.append(review)
 
+    json_data = json.dumps(reviews[:100])
+    with open('reviews.json', 'w') as file:
+        file.write(json_data)
+
+
+scrape_imdb_reviews(user_input)

5d16ca428aaf48552c9a07415f66b0099d2256cf
Dev Joshi <GalaxiDev0@gmail.com>
2024-07-26 16:16:15 -0400

    debugging json formatting problem

--- scraping.py
+++ scraping.py
@@ -11,7 +11,7 @@ import pandas
 import json
 import os
 from dotenv import load_dotenv
-
+from sentiment import analyze_sentiment
 
 load_dotenv("env/.env")
 api_key = os.getenv('API_KEY')
@@ -39,14 +39,25 @@ def scrape_imdb_reviews(movie_id):
     reviews_soup = BeautifulSoup(reviews_response.text, 'html.parser')
     review_containers = reviews_soup.find_all('div', class_='review-container')
 
+
     reviews = []
     for container in review_containers:
         review = container.find('div', class_='text show-more__control').text.strip()
         reviews.append(review)
+    return reviews
 
-        json_data = json.dumps(reviews[:100], indent=4, seperators=(',', ':'))
-        with open('reviews.json', 'w') as file:
-            file.write(json_data)
+def store_reviews(reviews):
+    analyzed_reviews = []
+    for review in reviews:
+        sentiment_score = analyze_sentiment(review)
+        analyzed_reviews.append({"review" : review, "sentiment_score" : analyze_sentiment(review)})
+        for i in range(len(reviews)):
+            print(reviews[i])
+        return analyzed_reviews
+
+def convert_to_json(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
 
 def main():
     movie_name = input("Enter the name of the movie: ")
@@ -55,7 +66,9 @@ def main():
     if imdb_id.startswith('tt'):
         print(f"The IMDb ID for '{movie_name}' is: {imdb_id}")
         print(f"https://www.imdb.com/title/{imdb_id}/reviews/?ref_=tt_ov_rt")
-        scrape_imdb_reviews(imdb_id)
+        reviews = scrape_imdb_reviews(imdb_id)
+        cleaned_reviews = store_reviews(reviews)
+        convert_to_json(cleaned_reviews, f"reviews/{movie_name}.json")
     else:
         print(imdb_id)
 

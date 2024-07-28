843d4e1ab822c8de448e6f56aeff9b1cededde59
Dev Joshi <GalaxiDev0@gmail.com>
2024-07-28 18:50:49 -0400

    added react template for the frontend of the website

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,15 @@
 # localhost:8000/create_contact
+from flask import request, jsonify
+from config import app, db
+from models import Contact
+
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    return jsonify([contact.to_json() for contact in contacts])
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)

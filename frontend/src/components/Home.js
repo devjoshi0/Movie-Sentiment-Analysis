68173c45b4acdd91d79f08ed32695e91a9837e6a
Dev Joshi <GalaxiDev0@gmail.com>
2024-07-31 21:11:27 -0400

    setup for front end

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import MovieCard from './MovieCard';
+import './Home.css';
+
+const Home = () => {
+    const [trending, setTrending] = useState([]);
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [popularTv, setPopularTv] = useState([]);
+    const [newReleases, setNewReleases] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const trendingResponse = await axios.get('HTTP://localhost:5000/');
+            const popularMoviesResponse = await axios.get('http://localhost:5000/popular_movies');
+            const popularTvResponse = await axios.get('http://localhost:5000/popular_tv');
+            const newReleasesResponse = await axios.get('http://localhost:5000/new_releases');
+
+            setTrending(trendingResponse.data.trending);
+            setPopularMovies(popularMoviesResponse.data);
+            setPopularTv(popularTvResponse.data);
+            setNewReleases(newReleasesResponse.data);
+    };
+    fetchData();
+}, []);
+
+    return (
+        <div className="home">
+            <h1>Trending</h1>
+            <div className="movie-container">
+                {trending.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+            <h1>Popular Movies</h1>
+            <div className="movie-container">
+                {popularMovies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+            <h1>Popular TV Shows</h1>
+            <div className="movie-container">
+                {popularTv.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+            <h1>New Releases</h1>
+            <div className="movie-container">
+                {newReleases.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
